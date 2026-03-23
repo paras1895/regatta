cc3cf338c0423410c7a4e492c8c2c9dbf0ee09cb
paras <parasxyz1234@gmail.com>
2026-03-23 19:03:01 +0530

    remove countdown

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,7 +8,6 @@ import {
   AnimatePresence,
 } from "framer-motion";
 import WaveText from "./WaveText";
-import Countdown from "./Countdown";
 
 const images = ["/events/kayak.jpg", "/events/shell.jpg", "/events/punt.jpg"];
 
@@ -74,8 +73,6 @@ const Hero: React.FC = () => {
               Where tradition flows forward. India's oldest college festival
               showcasing the spirit of generations on the Mula River.
             </motion.p>
-
-            <Countdown />
           </div>
 
           <div className="lg:col-span-5 relative flex justify-center">
